5429cd61cdf4f8b7d795e4dd4bf7398d31a780c3
maxoldgood <maxoldgood@gmail.com>
2023-12-25 12:49:06 +0600

    Header UI component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,13 @@
+import { CssBaseline } from '@mui/material'
+import { Header } from './Common/Header'
+
 function App() {
-  return ssssss
+  return (
+    <>
+      <CssBaseline>
+        <Header />
+      </CssBaseline>
+    </>
+  )
 }
 export default App
